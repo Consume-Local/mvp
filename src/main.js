7983b5444b8e20c5570e7b44b61a05bf5dfaf092
José M. Gulias Lugo <chepetime@users.noreply.github.com>
2020-03-24 19:13:07 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 import App from './App.vue'
 import router from './router'
 import store from './store';
-import Vuex from 'vuex'
+
 import './registerServiceWorker'
 
 import '@/assets/css/tailwind.css'
